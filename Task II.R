d9ccc60a52766a74e3a58c9b6d3b54923bfda358
Junhao Zhang <70535242+Zjh980709@users.noreply.github.com>
2020-12-13 01:27:31 +0530

    Add files via upload

---
+++ Task II.R
@@ -0,0 +1,31 @@
+## Task II
+attach(Task_II)
+head(Task_II)
+#Covariance matrix of the variables
+S <- cov(Task_II)
+
+#Compute the eigenvalues and corresponding eigenvectors of S.
+
+s.eigen <- eigen(S)
+s.eigen
+
+#proportion of the total variance explained by the components.
+
+for (s in s.eigen$values) {
+  print(s / sum(s.eigen$values))
+}
+
+#A scree graph of the eigenvalues 
+
+plot(s.eigen$values, xlab = 'Eigenvalue Number', ylab = 'Eigenvalue Size', main = 'Scree Graph')
+lines(s.eigen$values)
+
+
+s.eigen$vectors
+
+#Visualize
+library(ggfortify)
+pca.plot <- autoplot(pilots.pca, data = pilots, colour = 'Group')
+pca.plot
+ggplot(Task, aes(x=PC1, y=PC2, color=Group)) + 
+  geom_point()
